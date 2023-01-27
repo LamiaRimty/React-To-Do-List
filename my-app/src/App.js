5810f545e34119fd1185cd3c045e515293671e06
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-01-27 23:07:41 +0100

    Create a form class

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,21 @@
 function App() {
   return (
     <div className="container">
-      </div>
+      <div className="heading">
+       <h1>To-Do-List</h1> 
+       </div>
+         <div className="form">
+           <input type="text"/>
+             <button>
+               <span>Add</span>
+             </button>
+         </div>
+          <div>
+           <ul>
+              <li>A Item</li>
+           </ul>
+          </div>
+    </div>
   );
 }
 
