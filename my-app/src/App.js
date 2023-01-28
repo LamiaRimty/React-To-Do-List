cab15b2db7f17d5439780d251f32e9651ce466b1
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-01-28 23:14:10 +0100

    Use UseState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,37 @@
+import { useState } from "react";
 
 
 function App() {
+  const [inputText,setInputText]=useState("");
+  
+  
+  function handleClick(event){
+    event.preventDefault();
+    console.log("Submitted");
+  }
+
+  function handleChange(event){
+    setInputText(event.target.value);
+  }
+
   return (
     <div className="container">
       <div className="heading">
-       <h1>To-Do-List</h1> 
+       <h1>What's your Plan Today?</h1> 
        </div>
-         <div className="form">
-           <input type="text"/>
+         <form onSubmit={handleClick}>
+           <input
+           onChange={handleChange}
+            type="text"
+            placeholder="Add a todo"
+            value={inputText}
+            name="text"
+            //className="todoInput"
+            />
              <button>
                <span>Add</span>
              </button>
-         </div>
+         </form>
           <div>
            <ul>
               <li>A Item</li>
