ce5d2971550591a596b1914eaaa7b57ed0e2eb95
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-01-29 23:25:31 +0100

    Apply line-Through to each item

--- my-app/src/components/ToDoItem.jsx
+++ my-app/src/components/ToDoItem.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-
-
+import { useState } from "react";
 
 function ToDoItem(props){
+    const [isDone,setIsDone]=useState(false);
+    function handleClick(){
+        setIsDone((prevValue)=>{
+            return !prevValue;
+        });
+    }
+
     return ( 
-        <div>
-             <li>{props.text}</li>
+        <div onClick={handleClick}>
+             <li style={{textDecoration:isDone? "line-through":"none" }} >{props.text}</li>
         </div>
     );   
 }
