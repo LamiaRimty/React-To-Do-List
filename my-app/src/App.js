22fbb836a4026c8e4042a388de3f2b6bf7b0fd23
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-01-29 12:08:12 +0100

    Current value added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,46 +1,44 @@
 import { useState } from "react";
 
-
 function App() {
-  const [inputText,setInputText]=useState("");
-  const [todos,SetTodos]=useState([]);
-  
-  function addTodo(event){
-    SetTodos((prevTodos)=>{
-      return[...prevTodos,inputText];
-    })
+const [inputText,setInputText]=useState("");
+const [todoItems,SetTodoItems]=useState([]);
 
-  }
+function handleChange(event){
+  const newValue=event.target.value;
+  setInputText(newValue);
+}
 
-  function handleChange(event){
-    setInputText(event.target.value);
-  }
-
+function addTodoItem(){
+  SetTodoItems((prevValue)=>{ 
+    return [...prevValue,inputText]
+  });
+  setInputText("");
+}
   return (
     <div className="container">
       <div className="heading">
        <h1>What's your Plan Today?</h1> 
        </div>
-         <form>
+         <div>
            <input
            onChange={handleChange}
+            value={inputText}
             type="text"
             placeholder="Add a todo"
-            value={inputText}
+           
             name="text"
-            //className="todoInput"
             />
-             <button type="submit"
-             onClick={addTodo} >
+             <button onClick={addTodoItem} >
                <span>Add</span>
              </button>
          
           <div>
            <ul>
-             {todos.map((todoItem)=><li>{todoItem}</li>)}
+            { todoItems.map((todoItem)=><li>{todoItem}</li>)}
            </ul>
           </div>
-          </form>
+          </div>
     </div>
   );
 }
